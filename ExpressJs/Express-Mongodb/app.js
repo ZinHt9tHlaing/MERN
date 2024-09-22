4c46967997090384934dd7c677180094657c3d6b
ZinHt9tHlaing <yngWIE@9420>
2024-09-22 17:19:37 +0630

    Work with more than one model

--- ExpressJs/Express-Mongodb/app.js
+++ ExpressJs/Express-Mongodb/app.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const postRoute = require("./routes/post");
-const router = require("./routes/admin");
+const bodyParser = require("body-parser");
 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
-const bodyParser = require("body-parser");
+const postRoute = require("./routes/post");
+const router = require("./routes/admin");
+
+const User = require("./models/user");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  User.findById("66efe056b50bc6396a99e91d").then((foundUser) => {
+    req.user = foundUser; // todo: custom request
+    next();
+  });
+});
 
 app.use(postRoute);
 app.use("/admin", router);
@@ -25,5 +34,16 @@ mongoose
     app.listen(PORT, () => {
       console.log(`Server started on port http://localhost:${PORT}`);
     });
+    return User.findOne().then((user) => {
+      if (!user) {
+        User.create({
+          username: "coder",
+          email: "admin@gmail.com",
+          password: "12345",
+        });
+      }
+      return user;
+    });
   })
+  .then((result) => console.log(result))
   .catch((err) => console.log(err));
